3f4a8e4a174fbe93c2aef02dc03264ffd0eade05
Bret Gourdie <AdjectiveNounIsAdjective@gmail.com>
2015-04-18 17:07:05 -0400

    Enter button submits message
    No need to click send anymore

--- Auction 'Em All/public/js/index.js
+++ Auction 'Em All/public/js/index.js
@@ -1,11 +1,18 @@
 ﻿var socket = io();
-$('#send-message-btn').click(function () {
+$(document).keypress(function (e) {
+    if (e.which == 13) {
+        handleMessageBox();
+    }
+});
+$('#send-message-btn').click(handleMessageBox());
+socket.on('chat', function (msg) {
+    $('#messages').append($('<p>').text(msg));
+});
+
+function handleMessageBox(){
     var msg = $('#message-box').val();
     socket.emit('chat', msg);
     $('#messages').append($('<p>').text(msg));
     $('#message-box').val('');
     return false;
-});
-socket.on('chat', function (msg) {
-    $('#messages').append($('<p>').text(msg));
-});
+}
