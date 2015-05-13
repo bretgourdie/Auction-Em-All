5c2b00e86a792f1c41441fe9d6d4a425df915076
bretgourdie <adjectivenounisadjective@gmail.com>
2015-05-13 12:54:43 -0400

    Changed port number for OpenShift
    Maybe this will work................

--- Auction 'Em All/app.js
+++ Auction 'Em All/app.js
@@ -29,7 +29,7 @@ var currentCheckedIn = 0;
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
